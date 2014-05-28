hjwp/book-example

Strip down lists/urls to just its 3. --ch06l045--

lists/urls.py
@@ -1,11 +1,7 @@
-from django.conf.urls import patterns, include, url
-
-# from django.contrib import admin
-# admin.autodiscover()
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/(\d+)/$', 'lists.views.view_list', name='view_list'),
-    url(r'^lists/(\d+)/new_item$', 'lists.views.add_item', name='add_item'),
-    url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
+    url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
+    url(r'^(\d+)/new_item$', 'lists.views.add_item', name='add_item'),
+    url(r'^new$', 'lists.views.new_list', name='new_list'),
 )

Oniwa/TDD-for-python

lists/urls.py
@@ -1,10 +1,7 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/(\d+)/$', 'lists.views.view_list', name='view_list'),
-    url(r'^lists/(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
-    url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
+    url(r'^(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
+    url(r'^new$', 'lists.views.new_list', name='new_list'),
 )