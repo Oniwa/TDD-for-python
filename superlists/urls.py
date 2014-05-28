hjwp/book-example

New url for add item view. --ch06l039--

superlists/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^$', 'lists.views.home_page', name='home'),
     url(r'^lists/(\d+)/$', 'lists.views.view_list', name='view_list'),
+    url(r'^lists/(\d+)/new_item$', 'lists.views.add_item', name='add_item'),
     url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
 
     # Uncomment the admin/doc line below to enable admin documentation:

Oniwa/TDD-for-python

superlists/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^$', 'lists.views.home_page', name='home'),
     url(r'^lists/(\d+)/$', 'lists.views.view_list', name='view_list'),
+    url(r'^lists/(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
     url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
     # url(r'^admin/', include(admin.site.urls)),
 )