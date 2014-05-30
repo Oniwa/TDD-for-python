Radcliffe/book-example

wrong call to get with no args. --ch16l033--

accounts/authentication.py
@@ -22,5 +22,5 @@ class PersonaAuthenticationBackend(object):
 
 
     def get_user(self, email):
-        pass
+        return User.objects.get()
 

Oniwa/TDD-for-python

accounts/authentication.py
@@ -22,5 +22,5 @@ class PersonaAuthenticationBackend(object):
 
 
     def get_user(self, email):
-        pass
+        return User.objects.first()
 