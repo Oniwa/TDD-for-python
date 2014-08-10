05663b950673ef83bc76672bc61c4cf1cea629c1
John Barksdale <jdbarks@gmail.com>
2014-08-09 21:20:43 -0500

    Finished refactoring

--- lists/tests.py
+++ lists/tests.py
@@ -48,16 +48,6 @@ class HomePageTest(TestCase):
         home_page(request)
         self.assertEqual(Item.objects.count(), 0)
 
-    def test_home_page_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        request = HttpRequest()
-        response = home_page(request)
-
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
-
 
 class ItemModelTest(TestCase):
 
@@ -81,6 +71,9 @@ class ItemModelTest(TestCase):
 
 class ListViewTest(TestCase):
 
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
     def test_displays_all_items(self):
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
--- lists/views.py
+++ lists/views.py
@@ -7,12 +7,10 @@ def home_page(request):
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/lists/the-only-list-in-the-world/')
 
-    #TODO: Display multiple items in the table
     #TODO: Support more than one list
-    items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'home.html')
 
 
 def view_list(request):
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
