9653f0a1fd0fe562c30a50f0704fc442ffe6642a
John Barksdale <jdbarks@gmail.com>
2014-08-09 21:03:13 -0500

    Added URL /lists/the-only-list-in-the-world/.  Commiting before refactoring

--- lists/views.py
+++ lists/views.py
@@ -5,9 +5,14 @@ from lists.models import Item
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
 
     #TODO: Display multiple items in the table
     #TODO: Support more than one list
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
+
+
+def view_list(request):
     items = Item.objects.all()
     return render(request, 'home.html', {'items': items})
--- superlists/urls.py
+++ superlists/urls.py
@@ -4,8 +4,9 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'lists.views.home_page', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list',
+        name='view_list'
+        ),
     #url(r'^admin/', include(admin.site.urls)),
 
 )
